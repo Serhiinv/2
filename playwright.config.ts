1bfa1027f0c203c4619e332937c84120b31810d3
sn <serhii.nv@gmail.com>
2025-08-12 08:09:53 +0100

    new allure

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,15 +10,19 @@ export default defineConfig({
   forbidOnly: typeof process !== 'undefined' && !!process.env.CI,
   retries: typeof process !== 'undefined' && process.env.CI ? 2 : 0,
   workers: typeof process !== 'undefined' && process.env.CI ? 1 : undefined as unknown as number,
-  // reporter: [
-  //   ['list'],
-  //   ['allure-playwright', {
-  //     detail: true,
-  //     outputFolder: 'allure-results',
-  //     suiteTitle: false
-  //   }]
-  // ],
-  reporter: [['html', { open: 'never', outputFolder: 'playwright-report' }]],
+  // To use Allure
+  reporter: [
+    ['list'],
+    ['allure-playwright', {
+      detail: true,
+      outputFolder: 'allure-results',
+      suiteTitle: false
+    }]
+  ],
+
+  // To use playwright-report
+  // reporter: [['html', { open: 'never', outputFolder: 'playwright-report' }]],
+  
   use: {
     trace: 'on-first-retry',
     baseURL: 'https://playwright.dev/',
